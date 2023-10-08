1b4a86544feb9333aba2f4f36ffd9669c8f2b42a
nikita12328 <72278899+nikita12328@users.noreply.github.com>
2023-10-08 12:54:45 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
